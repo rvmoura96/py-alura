03b65790b7f14468ab8e07eb9aabdac41d96f8e4
Renan Moura <rvmoura.96@gmail.com>
2018-02-01 21:58:57 -0200

    exercicios parciais do modulo 5

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,10 +5,13 @@ print('*' * 32)
 
 secret_number = 42
 attempts = 3
-shout = 1
-while(shout <= attempts):
-    print("Rodada", shout, " de ", attempts)
-    user_number = int(input('Insira seu número: '))
+
+for i in range(1, attempts + 1):
+    print("Tentativa {} de {}".format(i, attempts))
+    user_number = int(input('Insira seu número entre 1 e 100: '))
+    if(user_number < 1 or user_number > 100):
+        print('Você deve digitar um número entre 1 e 100!')
+        continue
     hit = user_number == secret_number
     lesser = user_number < secret_number
     bigger = user_number > secret_number
@@ -19,7 +22,6 @@ while(shout <= attempts):
         print('Você errou, seu chute foi maior que o número secreto')
     elif(lesser):
         print('Você errou, seu chute foi menor que o número secreto')
-    shout = shout + 1
 
 print('Suas tentativas acabaram.')
 print('Fim do jogo.')
