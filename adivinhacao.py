0e92b9edfb420ab15d33950729c30c15082fb1cd
Renan Moura <rvmoura.96@gmail.com>
2018-02-01 21:00:01 -0200

    Conclusão da primeira parte da aula 4

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,8 +5,9 @@ print('*' * 32)
 
 secret_number = 42
 attempts = 3
-
-while(attempts > 0):
+shout = 1
+while(shout <= attempts):
+    print("Rodada", shout, " de ", attempts)
     user_number = int(input('Insira seu número: '))
     hit = user_number == secret_number
     lesser = user_number < secret_number
@@ -18,7 +19,7 @@ while(attempts > 0):
         print('Você errou, seu chute foi maior que o número secreto')
     elif(lesser):
         print('Você errou, seu chute foi menor que o número secreto')
-    attempts = attempts - 1
+    shout = shout + 1
 
 print('Suas tentativas acabaram.')
 print('Fim do jogo.')
